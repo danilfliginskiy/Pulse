cbc861205addccbc311301a3ad567209b99f7bf0
Danil <fliginskiy@gmail.com>
2020-03-29 21:39:56 +0300

    add tabs. need fix all items and add work of tabs

--- src/js/script.js
+++ src/js/script.js
@@ -15,3 +15,16 @@ var slider = tns({
   document.querySelector('.next').addEventListener('click', function () {
     slider.goTo('next');
   })
+
+  var content = document.querySelector('.catalog-item__content');
+  var list = document.querySelector('.catalog-item__list');
+
+  document.querySelector('.catalog-item__link').addEventListener('click', function () {
+    list.classList.add('catalog-item__list_active'),
+    content.classList.remove('catalog-item__content_active');
+  })
+
+  document.querySelector('.catalog-item__back-link').addEventListener('click', function () {
+    list.classList.remove('catalog-item__list_active'),
+    content.classList.add('catalog-item__content_active');
+  })
