52806edcd042028d7509a9215894ba75b08523ba
danilfliginskiy <58595331+danilfliginskiy@users.noreply.github.com>
2020-04-17 13:54:13 +0300

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,15 @@ const sass          = require('gulp-sass');
 const rename        = require("gulp-rename");
 const autoprefixer  = require('gulp-autoprefixer');
 const cleanCSS      = require('gulp-clean-css');
+const imagemin      = require('gulp-imagemin');
+const htmlmin       = require('gulp-htmlmin');
 
 // Static server
 gulp.task('server', function() {
 
     browserSync({
         server: {
-            baseDir: "src"
+            baseDir: "dist"
         }
     });
 
@@ -23,12 +25,40 @@ gulp.task('styles', function() {
         .pipe(rename({suffix: '.min', prefix: ''}))
         .pipe(autoprefixer())
         .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest("src/css"))
+        .pipe(gulp.dest("dist/css"))
         .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function() {
-    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
+    gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel('styles'));
+    gulp.watch("src/*.html").on('change', gulp.parallel('html'));
 })
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
+gulp.task('html', function(){
+    return gulp.src("src/*.html")
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest("dist/"))
+});
+
+gulp.task('scripts', function(){
+    return gulp.src("src/js/**/*.js")
+        .pipe(gulp.dest("dist/js"))
+});
+
+gulp.task('fonts', function(){
+    return gulp.src("src/fonts/**/*")
+        .pipe(gulp.dest("dist/fonts"))
+});
+
+gulp.task('images', function(){
+    return gulp.src("src/images/**/*")
+        .pipe(imagemin())
+        .pipe(gulp.dest("dist/images"))
+});
+
+gulp.task('mailer', function(){
+    return gulp.src("src/mailer/**/*")
+        .pipe(gulp.dest("dist/mailer"))
+});
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'scripts', 'fonts', 'images', 'mailer', 'html'));
